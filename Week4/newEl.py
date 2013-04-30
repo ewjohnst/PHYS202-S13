9070de71c341ef6593e9d3c27a964247c2d1961d
Evan Johnston <ewjohnst@calpoly.edu>
2013-04-29 17:53:50 -0700

    finished notes

--- Week4/newEl.py
+++ Week4/newEl.py
@@ -1,15 +1,15 @@
 from math import sqrt
 import numpy as np
 
-def pointPotential(x,y,q1,posx,posy):
+def pointPotential(x,y,q,posx,posy):
 	"""defining constants and finds potential using distance to particle for a positive e"""       
-	k=8.98e9; q1=1.6e-19                         
-        PointP = (k*q)/np.sqrt((x-posx)**2+(y-posy)**2)
+	k=8.98e9; q = 1.6e-19                       
+        PointP = (k*q/np.sqrt((x-posx)**2+(y-posy)**2))
         return PointP
 
 
-def dipolePotential(x,y,qd, d):
+def dipolePotential(x,y,q, d):
     """finds the potential between two charges"""
-    k=8.98e9; qd = int()                          
-    Vdipole = (k*q1/np.sqrt(x**2+(y-(d/2))**2)) - (k*q1/np.sqrt(x**2+(y+(d/2))**2)) 
+    k=8.98e9                       
+    Vdipole = (k*q/np.sqrt(y**2+(x-(d/2))**2)) - (k*q/np.sqrt(y**2+(x+(d/2))**2)) 
     return Vdipole                                                           
